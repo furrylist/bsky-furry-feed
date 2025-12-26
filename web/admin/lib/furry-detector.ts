fe56bfa9ae1e9715dd5288202fc4c45500c48725
Kev <kevslashnull@gmail.com>
2025-12-26 01:58:51 +0100

    Allow space between theta-delta

--- web/admin/lib/furry-detector.ts
+++ web/admin/lib/furry-detector.ts
@@ -12,7 +12,7 @@ export function isProbablyFurry(profile?: ProfileViewMinimal): boolean {
 
   // ∆ (increment operator) and Δ (delta)
   // Θ (uppercase theta) and θ (lowercase theta)
-  const therian = /(Θ|θ)(∆|Δ)/i;
+  const therian = /(Θ|θ)\s*(∆|Δ)/i;
 
   if (profile?.displayName?.match(therian)) {
     return true;
