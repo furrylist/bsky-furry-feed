furrylist/bsky-furry-feed

commit b49913dd8f7efc36ac5006c506011ac9cec21626

refactor: use go-native slices/maps packages
We no longer need to use the experimental packages because Go provides
these packages in the stdlib now.

ingester/handle_feed_post.go
@@ -3,6 +3,8 @@ package ingester
 import (
 	"context"
 	"fmt"
+	"maps"
+	"slices"
 	"strings"
 	"time"
 
@@ -10,7 +12,6 @@ import (
 	"github.com/srinathh/hashtag"
 	"github.com/strideynet/bsky-furry-feed/bluesky"
 	"github.com/strideynet/bsky-furry-feed/store"
-	"golang.org/x/exp/maps"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
@@ -97,7 +98,7 @@ func normalizeHashtags(hashtags []string, langs []string) []string {
 		}
 	}
 
-	return maps.Keys(hashtagsSet)
+	return slices.Collect(maps.Keys(hashtagsSet))
 }
 
 func (fi *FirehoseIngester) handleFeedPostCreate(

api/moderation.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"slices"
 	"time"
 
 	"github.com/strideynet/bsky-furry-feed/bfflog"
 	"github.com/strideynet/bsky-furry-feed/bluesky"
-	"golang.org/x/exp/slices"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"connectrpc.com/connect"

cmd/bffctl/main.go
@@ -6,8 +6,7 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
-
-	"golang.org/x/exp/slices"
+	"slices"
 
 	"github.com/joho/godotenv"
 	"github.com/strideynet/bsky-furry-feed/bfflog"

go.mod
@@ -30,7 +30,6 @@ require (
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.41.0
 	go.opentelemetry.io/otel/sdk v1.41.0
 	go.yaml.in/yaml/v2 v2.4.3
-	golang.org/x/exp v0.0.0-20260112195511-716be5621a96
 	golang.org/x/sync v0.19.0
 	golang.org/x/sys v0.41.0
 	google.golang.org/protobuf v1.36.11
@@ -127,6 +126,7 @@ require (
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.41.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.9.0 // indirect
 	go.uber.org/zap v1.27.1 // indirect
+	golang.org/x/exp v0.0.0-20260112195511-716be5621a96 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20260209200024-4cfbd4190f57 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260209200024-4cfbd4190f57 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect

commit a4a424cfaff4b67b00db3ed6b92efbe232297e40

fix: posts without hashtags aren't inserted
The refactor in b49913d caused  `normalizeHashtags` to return `nil`
instead of an empty slice when `hashtagsSet` is empty.

This would cause a non-null constraint error when storing the post.

See https://github.com/furrylist/bsky-furry-feed/actions/runs/24638651727/job/72038563916#step:5:454

ingester/handle_feed_post.go
@@ -98,6 +98,11 @@ func normalizeHashtags(hashtags []string, langs []string) []string {
 		}
 	}
 
+	// we should never return nil due to pg non-null constraint
+	if len(hashtagsSet) == 0 {
+		return []string{}
+	}
+
 	return slices.Collect(maps.Keys(hashtagsSet))
 }
 